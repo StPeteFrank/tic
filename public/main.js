17943ca86efebb68193f3ed92678f61117c7a153
Frank Diaz <diazfrankp@gmail.com>
2018-11-08 15:28:25 -0500

    example

--- public/main.js
+++ public/main.js
@@ -2,50 +2,51 @@ const main = () => {
   document.querySelector('h1').textContent += '?'
 }
 // How to play tic tac toe
+
 // The game must be two players
 // Decide who will be Player X
 // Decide who will be Player O
-// Create a nine box grid with space to mark the boxes
-// To win player will need to have three boxes marked in a horizontal, vertical, or diagonal line
-// Cannot win by having a line that is not  horizontal, vertical, or diagonal
-// Players cannot mark closed boxes
 
-// Determine where the possible winning lines are
-// The game board is a square made up of nine squares stacked 3 wide and 3 high
-// A win is defined by a player occupying every space in a row, column, or diagonal line
+// The game board is a square made up of nine squares called spaces
+// The spaces are arranged in three rows
+// The first row has spaces 1 through 3 and are numbered left to right
+// The second row has spaces 4 through 6 and are numbered left to right
+// The first row has spaces 7 through 9 and are numbered left to right
 
-// Possible wins for player X
-// Rows:
-// The first row is comprised of spaces 1, 2, 3
-// The second row is comprised of spaces 4, 5, 6
-// The third row is comprised of spaces 7,8,9
-// Columns:
-// The first column is comprised of spaces 1,4,7
-// The second column is comprised of spaces 2,5,8
-// The third column is comprised of spaces 3,6,9
-// Diagonals:
-// The first diagonal is comprised of spaces 1,5,9
-// The second diagonal is comprised of spaces 3,5,7
+// Determine what combinations of spaces will result in a win:
 
-// Possible wins for player X
-// Rows:
-// The first row is comprised of spaces 1, 2, 3
-// The second row is comprised of spaces 4, 5, 6
-// The third row is comprised of spaces 7,8,9
-// Columns:
-// The first column is comprised of spaces 1,4,7
-// The second column is comprised of spaces 2,5,8
-// The third column is comprised of spaces 3,6,9
-// Diagonals:
-// The first diagonal is comprised of spaces 1,5,9
-// The second diagonal is comprised of spaces 3,5,7
+// Possible wins for Player X:
 
-// Player X makes choice
-// Player O makes choice
-// Players alternate moves until one of the possible wins conditions is met
-// Remember which player marked which box
-// If none of the possible win conditions are met the game results in a tie
-// If a winner is determined or game results in a tie they may play again
+// if space 1 is an X and space 2 is an X and space 3 is an X: X wins
+// if space 4 is an X and space 5 is an X and space 6 is an X: X wins
+// if space 7 is an X and space 8 is an X and space 9 is an X: X wins
+// if space 1 is an X and space 4 is an X and space 7 is an X: X wins
+// if space 2 is an X and space 5 is an X and space 8 is an X: X wins
+// if space 3 is an X and space 6 is an X and space 9 is an X: X wins
+// if space 1 is an X and space 5 is an X and space 9 is an X: X wins
+// if space 3 is an X and space 5 is an X and space 7 is an X: X wins
+
+// Possible wins for Player O:
+
+// if space 1 is an O and space 2 is an O and space 3 is an O: O wins
+// if space 4 is an O and space 5 is an O and space 6 is an O: O wins
+// if space 7 is an O and space 8 is an O and space 9 is an O: O wins
+// if space 1 is an O and space 4 is an O and space 7 is an O: O wins
+// if space 2 is an O and space 5 is an O and space 8 is an O: O wins
+// if space 3 is an O and space 6 is an O and space 9 is an O: O wins
+// if space 1 is an O and space 5 is an O and space 9 is an O: O wins
+// if space 3 is an O and space 5 is an O and space 7 is an O: O wins
+
+// Players can now begin to chose spaces
+// Player X chooses a space
+// Player O chooses a space
+// Remember which player marked which space
+// Players cannot choose spaces that have been chosen previously
+// Players alternate moves until one of the possible win conditions is met
+// A winner is determined if one of the win conditions is met
+// If all spaces are chosen and none of the possible win conditions are met the game results in a tie
+// If a winner is determined or game results in a tie the game is over
+// The players can chose to play again if they wish
 // Players may keep score if they wish
 
 document.addEventListener('DOMContentLoaded', main)
