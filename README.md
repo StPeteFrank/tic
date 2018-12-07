1833e536418e748bf1d10ec52fe5f6644c1fb950
Frank Diaz <diazfrankp@gmail.com>
2018-12-07 15:43:08 -0500

    Readme GIF added.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Tic Tac Toe
+# Tic-Tac-Toe
+
+<p align="center">
+<img src="src/TicTacToeGIF.gif">
+</p>
+
+## Description
+
+A Tic-Tac-Toe app built using simple JavaScript, HTML, and CSS.
+
+- JavaScript. A scripting or programming language that allows you to implement complex things on web pages — every time a web page does more than just sit there and display static information for you to look at — displaying timely content updates, interactive maps, animated 2D/3D graphics, scrolling video jukeboxes, etc. — you can bet that JavaScript is probably involved.
+
+https://www.javascript.com/
+
+- React. A declarative, efficient, and flexible JavaScript library for building user interfaces. It lets you compose complex UIs from small and isolated pieces of code called “components”.
+
+https://reactjs.org/
--- public/main.js
+++ public/main.js
@@ -1,28 +1,6 @@
 const main = () => {
   document.querySelector('h1').textContent
 }
-
-// NOTES -------------------------------------------------------------------------
-
-// How to play tic tac toe
-
-// The game must be two players
-// Decide who will be Player X
-// Decide who will be Player O
-
-// The game board is a square made up of nine squares called spaces
-// The spaces are arranged in three rows
-// The first row has spaces 1 through 3 and are numbered left to right
-// The second row has spaces 4 through 6 and are numbered left to right
-// The first row has spaces 7 through 9 and are numbered left to right
-
-// Determine what combinations of spaces will result in a win:
-
-// Possible wins for Player X:
-
-// END NOTES -------------------------------------------------------------------------
-
-// sets 1st player to either X or O - true is O, false is X 
 let playerOne = false
 
 document.querySelector('.reset-button').addEventListener('click', () => {
@@ -43,15 +21,15 @@ document.querySelector('.reset-button').addEventListener('click', () => {
 document.querySelectorAll('.playButton').forEach(button => {
   button.addEventListener('click', () => {
     let currentPlayer
-    // this checks the status of playerOne and if true - 
+    // this checks the status of playerOne and if true -
     // sets the value of current player to O
     // if false, sets if to X
-    playerOne ? currentPlayer = 'O' : currentPlayer = 'X'
+    playerOne ? (currentPlayer = 'O') : (currentPlayer = 'X')
     // changes the value of currentPlayer so it alternates
-    playerOne ? playerOne = false : playerOne = true
+    playerOne ? (playerOne = false) : (playerOne = true)
     // sets the text of the chosen button to value of currentPlayer (X or O)
     button.textContent = currentPlayer
-    // sets the "disabled" property of the chosen button to "disabled" so 
+    // sets the "disabled" property of the chosen button to "disabled" so
     // it can only get picked once
     button.setAttribute('disabled', 'disabled')
     // sets the value of the chosen object in the spaces object (see below)
@@ -63,22 +41,7 @@ document.querySelectorAll('.playButton').forEach(button => {
     checkIfBoardIsFull()
     checkWins(currentPlayer)
   })
-
 })
-
-// NOTES -------------------------------------------------------------------------
-// for each click, the corresponding space to the chosen one must be set to 
-// the currentPlayer at the time it was clicked
-
-// the checkWins function is run and passed the currentPlayer variable 
-// which checks the current state of spaces for a matching pattern
-
-// a new line has to be added to checkWins to check if all spaces are full to 
-// declare a draw
-//  END NOTES -------------------------------------------------------------------------
-
-// this stores the current state of the spaces
-// whether the space is open, X or O
 
 let spaces = {
   spaceOne: 'open',
@@ -101,64 +64,64 @@ const checkIfBoardIsFull = () => {
   }
 }
 
-const closeBoard = (player) => {
+const closeBoard = player => {
   document.querySelector('.win-status').textContent = `${player} wins!`
-  document.querySelectorAll('.playButton').forEach(button => button.setAttribute('disabled', 'disabled'))
+  document
+    .querySelectorAll('.playButton')
+    .forEach(button => button.setAttribute('disabled', 'disabled'))
 }
 
 // logic for determining winning spaces
-const checkWins = (player) => {
-  if (spaces.spaceOne === player && spaces.spaceTwo === player && spaces.spaceThree === player) {
+const checkWins = player => {
+  if (
+    spaces.spaceOne === player &&
+    spaces.spaceTwo === player &&
+    spaces.spaceThree === player
+  ) {
     closeBoard(player)
-  } else if (spaces.spaceFour === player && spaces.spaceFive === player && spaces.spaceSix === player) {
+  } else if (
+    spaces.spaceFour === player &&
+    spaces.spaceFive === player &&
+    spaces.spaceSix === player
+  ) {
     closeBoard(player)
-  } else if (spaces.spaceSeven === player && spaces.spaceEight === player && spaces.spaceNine === player) {
+  } else if (
+    spaces.spaceSeven === player &&
+    spaces.spaceEight === player &&
+    spaces.spaceNine === player
+  ) {
     closeBoard(player)
-  } else if (spaces.spaceOne === player && spaces.spaceFour === player && spaces.spaceSeven === player) {
+  } else if (
+    spaces.spaceOne === player &&
+    spaces.spaceFour === player &&
+    spaces.spaceSeven === player
+  ) {
     closeBoard(player)
-  } else if (spaces.spaceTwo === player && spaces.spaceFive === player && spaces.spaceEight === player) {
+  } else if (
+    spaces.spaceTwo === player &&
+    spaces.spaceFive === player &&
+    spaces.spaceEight === player
+  ) {
     closeBoard(player)
-  } else if (spaces.spaceThree === player && spaces.spaceSix === player && spaces.spaceNine === player) {
+  } else if (
+    spaces.spaceThree === player &&
+    spaces.spaceSix === player &&
+    spaces.spaceNine === player
+  ) {
     closeBoard(player)
-  } else if (spaces.spaceOne === player && spaces.spaceFive === player && spaces.spaceNine === player) {
+  } else if (
+    spaces.spaceOne === player &&
+    spaces.spaceFive === player &&
+    spaces.spaceNine === player
+  ) {
     closeBoard(player)
-  } else if (spaces.spaceThree === player && spaces.spaceFive === player && spaces.spaceSeven === player) {
+  } else if (
+    spaces.spaceThree === player &&
+    spaces.spaceFive === player &&
+    spaces.spaceSeven === player
+  ) {
     closeBoard(player)
   }
 }
-// NOTES -------------------------------------------------------------------------
-
-// if space 1 is an X and space 2 is an X and space 3 is an X: X wins
-// if space 4 is an X and space 5 is an X and space 6 is an X: X wins
-// if space 7 is an X and space 8 is an X and space 9 is an X: X wins
-// if space 1 is an X and space 4 is an X and space 7 is an X: X wins
-// if space 2 is an X and space 5 is an X and space 8 is an X: X wins
-// if space 3 is an X and space 6 is an X and space 9 is an X: X wins
-// if space 1 is an X and space 5 is an X and space 9 is an X: X wins
-// if space 3 is an X and space 5 is an X and space 7 is an X: X wins
-
-// Possible wins for Player O:
-
-// if space 1 is an O and space 2 is an O and space 3 is an O: O wins
-// if space 4 is an O and space 5 is an O and space 6 is an O: O wins
-// if space 7 is an O and space 8 is an O and space 9 is an O: O wins
-// if space 1 is an O and space 4 is an O and space 7 is an O: O wins
-// if space 2 is an O and space 5 is an O and space 8 is an O: O wins
-// if space 3 is an O and space 6 is an O and space 9 is an O: O wins
-// if space 1 is an O and space 5 is an O and space 9 is an O: O wins
-// if space 3 is an O and space 5 is an O and space 7 is an O: O wins
-
-// Players can now begin to chose spaces
-// Player X chooses a space
-// Player O chooses a space
-// Remember which player marked which space
-// Players cannot choose spaces that have been chosen previously
-// Players alternate moves until one of the possible win conditions is met
-// A winner is determined if one of the win conditions is met
-// If all spaces are chosen and none of the possible win conditions are met the game results in a tie
-// If a winner is determined or game results in a tie the game is over
-// The players can chose to play again if they wish
-// Players may keep score if they wish
-// END NOTES -------------------------------------------------------------------------
 
 document.addEventListener('DOMContentLoaded', main)
